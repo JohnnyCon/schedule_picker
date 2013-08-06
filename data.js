b5dc2a70fc5b7557ab80d5367761829862f86b5a
John Contreras <contrerasnet@gmail.com>
2013-08-06 14:08:14 -0500

    update the test data so picker1 and picker1 have different data sets

--- data.js
+++ data.js
@@ -1,10 +1,13 @@
 
-var schedule =
+var schedule
+var schedule2
+
+schedule =
 {
   "days": [
 
     {
-      "date" : "8/3/2013",
+      "date" : "Mon, 8/3/2013",
       "times" : [
         {
           "time" : "9:00AM - 9:50AM",
@@ -57,7 +60,451 @@ var schedule =
           ]
         },
         {
+          "time" : "11:00AM - 11:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 105
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 106
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 107
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 108
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 109
+            },
+          ]
+        },
+        {
+          "time" : "1:00PM - 1:50PM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 110
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 111
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 112
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 113
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 114
+            },
+          ]
+        }
+      ]
+
+    },
+
+
+
+    {
+      "date" : "Tue, 8/4/2013",
+      "times" : [
+        {
+          "time" : "8:00AM - 8:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 115
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 116
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 117
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 118
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 119
+            },
+          ]
+        },
+        {
           "time" : "9:00AM - 9:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 120
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 121
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 122
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 123
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 124
+            },
+          ]
+        },
+        {
+          "time" : "12:00PM - 12:50PM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 125
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 126
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 127
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 128
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 129
+            },
+          ]
+        }
+      ]
+
+    },
+
+
+
+
+
+    {
+      "date" : "Wed, 8/5/2013",
+      "times" : [
+        {
+          "time" : "9:00AM - 9:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 130
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 131
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 132
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 133
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 134
+            },
+          ]
+        },
+        {
+          "time" : "10:00AM - 10:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 135
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 136
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 137
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 138
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 139
+            },
+          ]
+        },
+        {
+          "time" : "11:00AM - 11:50AM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 140
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 141
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 142
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 143
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 144
+            },
+          ]
+        }
+      ]
+
+    }
+
+
+
+
+
+
+
+  ]
+}
+
+
+
+
+
+var schedule2 =
+{
+  "days": [
+
+    {
+      "date" : "Wed, 8/5/2013",
+      "times" : [
+        {
+          "time" : "2:00PM - 2:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -107,7 +554,57 @@ var schedule =
           ]
         },
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "3:00PM - 3:50PM",
+          "classes" : [
+            {
+              "instructor" : "Sebastian Thrun",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 100
+            },
+            {
+              "instructor" : "Andrew Ng",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 101
+            },
+            {
+              "instructor" : "Daphne Koller",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 102
+            },
+            {
+              "instructor" : "Alex Aiken",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 103
+            },
+            {
+              "instructor" : "Time Roughgarden",
+              "instructor_avatar" : "http://www.example.com",
+              "class_open" : true,
+              "slots_text" : "3 of 4 filled",
+              "slots_filled" : 3,
+              "slots_total" : 5,
+              "class_id" : 104
+            },
+          ]
+        },
+        {
+          "time" : "6:00PM - 6:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -163,10 +660,10 @@ var schedule =
 
 
     {
-      "date" : "8/4/2013",
+      "date" : "Thur, 8/6/2013",
       "times" : [
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "3:00PM - 3:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -216,7 +713,7 @@ var schedule =
           ]
         },
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "5:00PM - 5:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -266,7 +763,7 @@ var schedule =
           ]
         },
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "6:00PM - 6:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -324,7 +821,7 @@ var schedule =
 
 
     {
-      "date" : "8/5/2013",
+      "date" : "Fri, 8/7/2013",
       "times" : [
         {
           "time" : "9:00AM - 9:50AM",
@@ -377,7 +874,7 @@ var schedule =
           ]
         },
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "12:00PM - 12:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -427,7 +924,7 @@ var schedule =
           ]
         },
         {
-          "time" : "9:00AM - 9:50AM",
+          "time" : "1:00PM - 9:50PM",
           "classes" : [
             {
               "instructor" : "Sebastian Thrun",
@@ -488,3 +985,16 @@ var schedule =
 
   ]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
